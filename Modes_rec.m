76fb0493578c4312ee44264a60f9cee61c0ae166
minspan1999 <minspan@users.noreply.github.com>
2020-03-03 14:41:39 -0500

    formatted

--- Modes_rec.m
+++ Modes_rec.m
@@ -4,33 +4,33 @@ close all
 
 global L ne
 N = 10000;
-ne = 3.3;L = 1;k = 10*pi/L;h = L/N;x = -0.5*L:h:0.5*L;
+ne = 3.3; L = 1; k = 10 * pi / L; h = L / N; x = -0.5 * L:h:0.5 * L;
 % k = 2*pi/L;
-Ksi = diag((2*ne*x/L).^2 + 2i*1*ne/k/L); 
+Ksi = diag((2 * ne * x / L).^2 + 2i * 1 * ne / k / L);
 ks1 = diag(Ksi);
-H0 = diag(2*ones(N+1,1)) - diag(ones(N,1),-1) - diag(ones(N,1),1);
+H0 = diag(2 * ones(N + 1, 1)) - diag(ones(N, 1), -1) - diag(ones(N, 1), 1);
 % H0(1,1) = (2 - 0.0i);H0(N+1,N+1) = (2 - 0.0i);
-H0(1,1) = (2 - exp(-1i*ne*k*h));
-H0(N+1,N+1) = (2 - exp(-1i*ne*k*h));
-Hmm = H0 - h*h*k*k*Ksi.*eye(N + 1);
+H0(1, 1) = (2 - exp(-1i * ne * k * h));
+H0(N + 1, N + 1) = (2 - exp(-1i * ne * k * h));
+Hmm = H0 - h * h * k * k * Ksi .* eye(N + 1);
 % Hmm(1,1) = real(Hmm(1,1)) +0.01i;Hmm(N+1,N+1) = real(Hmm(N+1,N+1))+0.01i;
 
-[a,b] = eig(Hmm);
+[a, b] = eig(Hmm);
 figure;
-subplot(2,1,1);
-plot(real(diag(b)),imag(diag(b)),'b*');
-subplot(2,1,2);
-for kk = 1:1:N+1
-    plot(abs(a(:,kk)));
-    hold on; 
+subplot(2, 1, 1);
+plot(real(diag(b)), imag(diag(b)), 'b*');
+subplot(2, 1, 2);
+
+for kk = 1:1:N + 1
+    plot(abs(a(:, kk)));
+    hold on;
 end
 
-[c,d] = min(std(abs(a)));
+[c, d] = min(std(abs(a)));
 figure
-subplot(2,1,1);
-plot(abs(a(:,d)));
+subplot(2, 1, 1);
+plot(abs(a(:, d)));
 hold on;
-subplot(2,1,2);
-plot(angle(a(:,d)));
+subplot(2, 1, 2);
+plot(angle(a(:, d)));
 hold on;
-
